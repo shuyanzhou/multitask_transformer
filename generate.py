3a0e7fac28398245fb28df9f1a35cbb3069bb02f
alexisxy <alexisxy0418@gmail.com>
2019-04-27 23:14:58 -0400

    fix bug in genetator

--- generate.py
+++ generate.py
@@ -17,10 +17,8 @@ from fairseq.utils import import_user_module
 
 
 def retrieve_noisy_clean_outs(hypos, noisy_encoder_out, beam_size, max_len):
-    noisy_clean_outs = {}
-    noisy_clean_outs['noisy_encoder_out'] = noisy_encoder_out
-    noisy_clean_outs['clean_decoder_out'] = []
     all_outs = []
+    all_clean_scores = []
     _, batch_size, embed_size = noisy_encoder_out['encoder_out'].shape
     for beam_idx in range(beam_size):
         clean_decoder_tensor = torch.zeros((batch_size, max_len, embed_size))
@@ -37,16 +35,16 @@ def retrieve_noisy_clean_outs(hypos, noisy_encoder_out, beam_size, max_len):
         clean_decoder_tensor = torch.transpose(clean_decoder_tensor, 1, 0)
         if not clean_decoder_padding_mask.any():
             clean_decoder_padding_mask = None
-        noisy_clean_outs['clean_decoder_out'].append({
+        all_clean_scores.append(clean_scores)
+        cur_clean_decoder_out = {
             'decoder_out': clean_decoder_tensor,
             'decoder_padding_mask': clean_decoder_padding_mask,
-            'clean_scores': clean_scores
-        })
-        all_outs.append([copy.deepcopy(noisy_encoder_out), noisy_clean_outs['clean_decoder_out'][-1]])
-    return noisy_clean_outs, all_outs
+        }
+        all_outs.append([copy.deepcopy(noisy_encoder_out), copy.deepcopy(cur_clean_decoder_out)])
+    return all_clean_scores, all_outs
 
 
-def update_scores(translate_hypo, clean_scores, p=0.5):
+def update_scores(translate_hypo, clean_scores:torch.Tensor, p=0.5):
     for idx, sentence_hypo in enumerate(translate_hypo):
         for beam_hypo in sentence_hypo:
             beam_hypo["score"] = p * beam_hypo["score"] + (1 - p) * clean_scores[idx]
@@ -141,16 +139,18 @@ def main(args):
             gen_timer.start()
             # noisy_encoder_out is a dictionary
             hypos, noisy_encoder_out, max_len = task.inference_step(generator, models, sample, prefix_tokens)
-            noisy_clean_outs, all_outs = \
+            # all_clean_scores: a list of length beam_size, each with batch_size length
+            all_clean_scores, all_outs = \
                 retrieve_noisy_clean_outs(hypos, noisy_encoder_out, args.beam, max_len)
             all_hypos = []
             for beam_idx in range(args.beam):
                 translate_hypos, _, _ = task.inference_step(generator, models, sample=None, prefix_tokens=prefix_tokens,
                                                       bos_token=None, is_translation=True,
                                                       noisy_clean_outs=all_outs[beam_idx])
-                update_scores(translate_hypos, noisy_clean_outs["clean_decoder_out"][beam_idx]["clean_scores"])
-                all_hypos.append(translate_hypos)
-
+                # combine noisy - clean and noisy.clean - translate together
+                update_scores(translate_hypos, all_clean_scores[beam_idx])
+                all_hypos += translate_hypos
+            hypos = all_hypos
 
             num_generated_tokens = sum(len(h[0]['tokens']) for h in hypos)
             gen_timer.stop(num_generated_tokens)
